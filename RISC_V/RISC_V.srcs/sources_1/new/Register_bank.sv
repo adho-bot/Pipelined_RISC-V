a85462ec429283e24e9e28d532da0132279a58d5
Chi Ip <chi.ip@student.manchester.ac.uk>
2025-07-24 00:32:44 +0800

    first commit

---
+++ RISC_V/RISC_V.srcs/sources_1/new/Register_bank.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/23/2025 05:21:12 PM
+// Design Name: 
+// Module Name: Register_bank
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Register_bank(
+
+    );
+endmodule
